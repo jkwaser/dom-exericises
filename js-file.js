be9897fe43ef5fd38ff3f9bd33d39044b403fc84
jkwaser <jkwaser@gmail.com>
2022-03-27 23:17:48 +1000

    Changed one button to make background of button blue.

--- js-file.js
+++ js-file.js
@@ -1,5 +1,10 @@
-const btn = document.querySelector('#btn');
-btn.onclick = () => alert("Hello World");
+btn.addEventListener('click', function (e) {
+    console.log(e.target);
+  });
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+  });
 
 const btn2= document.querySelector('#btn2');
 btn2.addEventListener('click', () => {
