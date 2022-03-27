cbd5d159c000a56ef9801d655c7d056050bd47d3
jkwaser <jkwaser@gmail.com>
2022-03-27 23:16:38 +1000

    Exercises to do basic hello world buttons, understanding in-line JS, that in a different file and separation of concerns.

--- js-file.js
+++ js-file.js
@@ -1,7 +1,7 @@
-const container = document.querySelector('#container');
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
-
-container.appendChild(content);
+const btn2= document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+  alert("Hello World");
+});
